732a99e4de5fba2e8bb0ada8b7166200ee958f16
Matt Jhou <ycj1665@gmail.com>
2018-02-01 16:30:01 +0800

    better request

--- source/index.js
+++ source/index.js
@@ -6,26 +6,17 @@ $(document).ready(function () {
 });
 
 
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-       // Action to be performed when the document is read;
-    }
-};
-
 var cors = "https://cors-anywhere.herokuapp.com/"
 var getUrl = url => cors + url
-
-xhttp.open("GET", getUrl('https://news.ycombinator.com/'), true);
-xhttp.send();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-    console.log(this.responseText);
-    var xmlString = this.responseText
-        , parser = new DOMParser()
-        , doc = parser.parseFromString(xmlString, "text/xml");
-
+var url = 'https://www.kham.com.tw/application/UTK01/UTK0101_06.aspx?TYPE=1&CATEGORY=80'
+$.get(getUrl(url)).done(
+  (s) => {
     var el = document.createElement( 'div' );
-    el.innerHTML = this.responseText;
-    }
-};
+    el.innerHTML = s;
+    console.log($(el).find('title').text());
+  }
+).fail(
+  (e) => {
+    console.log('e: ', e);
+  }
+);
