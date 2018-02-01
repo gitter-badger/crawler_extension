8a33be3db2774193b68b708c4e31bc6356d45c25
weicheng2138 <weicheng2138@gmail.com>
2018-02-01 16:48:18 +0800

    test

--- source/index.js
+++ source/index.js
@@ -1,31 +1,30 @@
-$(document).ready(function () {
-    $('.radio-option').click(function () {
-        $(this).not(this).removeClass('click');
-        $(this).toggleClass("click");
-    });
-});
+// $(document).ready(function () {
+//     $('.radio-option').click(function () {
+//         $(this).not(this).removeClass('click');
+//         $(this).toggleClass("click");
+//     });
+// });
 
 
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
+    if (this.readyState === 4 && this.status === 200) {
        // Action to be performed when the document is read;
     }
 };
 
-var cors = "https://cors-anywhere.herokuapp.com/"
-var getUrl = url => cors + url
+var cors = "https://cors-anywhere.herokuapp.com/";
+var getUrl = url => cors + url;
 
-xhttp.open("GET", getUrl('https://news.ycombinator.com/'), true);
-xhttp.send();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-    console.log(this.responseText);
-    var xmlString = this.responseText
-        , parser = new DOMParser()
-        , doc = parser.parseFromString(xmlString, "text/xml");
-
-    var el = document.createElement( 'div' );
-    el.innerHTML = this.responseText;
+$.get(getUrl('https://www.kham.com.tw/application/UTK01/UTK0101_06.aspx?TYPE=1&CATEGORY=80')).done(
+    (s) => {
+        var el = document.createElement( 'div' );
+        el.innerHTML = s;
+        console.log($(el).find('title').text());
     }
-};
+).fail(
+    (e) => {
+        console.log('e: ', e);
+    }
+);
+
